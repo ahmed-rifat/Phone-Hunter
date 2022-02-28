6bd744001868ce3cf278b86834bf4530779d88e1
Ahmed Rifat <rifatbafsc@gmail.com>
2022-03-01 01:02:21 +0600

    columns added

--- phone-search.js
+++ phone-search.js
@@ -3,7 +3,32 @@ const loadPhone= () =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.data));
+    .then(data => displayPhone(data.data));
+    
 
 }
 
+const displayPhone = (phones)=>{
+    for (const phone of phones){
+        const phoneListContainer = document.getElementById('phone-list-container');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="row  mt-5">
+        <div class="col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="${phone.image}" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Name: ${phone.phone_name}</h5>
+                <p class="card-title"> Brand: ${phone.brand}<p>
+                <p class="card-title"> Model: ${phone.slug}<p>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+        </div>
+        
+        `
+        phoneListContainer.appendChild(div);
+
+        
+    }
+}
